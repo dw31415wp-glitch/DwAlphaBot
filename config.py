82f7dfba400d0e296bb92bc675c910bbb6c92a5b
Dw31415 <dw31415.wp@gmail.com>
2026-01-02 07:50:52 -0600

    About to run a couple of years

--- config.py
+++ config.py
@@ -8,9 +8,30 @@ DRY_RUN = False
 KILL_PAGE = 'User:Dw31415/kill'
 RESULTS_PAGE = 'https://en.wikipedia.org/wiki/User:DwAlphaBot/RfcEditStats'
 RESULT_PAGE_TITLE = 'User:DwAlphaBot/RfcEditStats'
+RAW_PAGES_LIST = [
+  "Requests_for_comment/Biographies",
+  "Requests_for_comment/Economy,_trade,_and_companies",
+  "Requests_for_comment/History_and_geography",
+  "Requests_for_comment/Language_and_linguistics",
+  "Requests_for_comment/Maths,_science,_and_technology",
+  "Requests_for_comment/Media,_the_arts,_and_architecture",
+  "Requests_for_comment/Politics,_government,_and_law",
+  "Requests_for_comment/Religion_and_philosophy",
+  "Requests_for_comment/Society,_sports,_and_culture",
+  "Requests_for_comment/Wikipedia_style_and_naming",
+  "Requests_for_comment/Wikipedia_policies_and_guidelines",
+  "Requests_for_comment/WikiProjects_and_collaborations",
+  "Requests_for_comment/Wikipedia_technical_issues_and_templates",
+  "Requests_for_comment/Wikipedia_proposals",
+  "Requests_for_comment/Unsorted",
+  "Requests_for_comment/User_names"
+]
+
 LIST_OF_RFC_PAGES = [
     'Wikipedia:Requests for comment/Politics, government, and law'
 ]
+
+YEARS_TO_PROCESS = [2022, 2023]
 MAX_RFC_PAGES_TO_PROCESS = 5
 JOB_TO_RUN = 'examine_history'  # Options: 'analyze_rfcs', 'examine_history'
 
--- examine_history.py
+++ examine_history.py
@@ -1,56 +1,143 @@
 
-from config import LIST_OF_RFC_PAGES, site
+import datetime
+from config import LIST_OF_RFC_PAGES, RAW_PAGES_LIST, YEARS_TO_PROCESS, site
 from pywikibot import Page
+from pywikibot.page import Revision
 from pywikibot.site import APISite
 from pywikibot.diff import html_comparator
 from pywikibot.time  import Timestamp
+import shelve
+
+
+class RevisionRun:
+    def __init__(self, year, page_title, timestamp = datetime.datetime.now().timestamp(), comment = ''):
+        self.type = 'r-1'
+        self.year = year
+        self.page_title = page_title
+        self.timestamp = timestamp
+        self.comment = comment
+        self.revisions_examined = 0
+        self.revisions_with_errors = 0
+
+    def get_key(self):
+        return f'{self.type}-{self.year}-{self.page_title}'
+    
+    def increment_revisions_examined(self):
+        self.revisions_examined += 1
+
+    def increment_revisions_with_errors(self):
+        self.revisions_with_errors += 1
+    
+    def get_complete_stats(self) -> tuple[str, dict]:
+        return (f"{self.type}-{self.year}-{self.page_title}-stats", {
+            'revisions_examined': self.revisions_examined,
+            'seconds_taken': datetime.datetime.now().timestamp() - self.timestamp,
+            'revisions_with_errors': self.revisions_with_errors,
+            'type': self.type,
+            'year': self.year,
+            'page_title': self.page_title,
+            'timestamp': self.timestamp,
+            'comment': self.comment
+        })
 
 
 def examine_history():
 
-    rfc_page = Page(site, LIST_OF_RFC_PAGES[0])
+    db = shelve.open('rfc.db', writeback=False)
 
-    if isinstance(site, APISite):
-        # Seems like we need to use site.loadrevisions to be able to filter by user
-        # Loads revisions into the page object
-        starttime: Timestamp = Timestamp(2024, 1, 1, 0, 0, 0)
-        endtime: Timestamp = Timestamp(2025, 1, 1, 0, 0, 0)
-        history = site.loadrevisions(page=rfc_page, user='Legobot', starttime=starttime, endtime=endtime, rvdir=True)
+    for year in YEARS_TO_PROCESS:
+        for raw_page_title in RAW_PAGES_LIST:
+            page_title = f"Wikipedia:{raw_page_title}"
+            run = RevisionRun(year=year, page_title=page_title, comment='Examining history for deletions')    
+            db[run.get_key()] = run
+            
+            try:
+                rfc_page = Page(site, page_title)
+            except Exception as e:
+                db.sync()
+                print(f"Error loading page {page_title}: {e}")
+                                
+            db[run.get_key()] = run
 
-    # TODO: Do I need to access _revisions or is there a better way?
+            if isinstance(site, APISite):
+                # Seems like we need to use site.loadrevisions to be able to filter by user
+                # Loads revisions into the page object
+                starttime: Timestamp = Timestamp(year, 1, 1, 0, 0, 0)
+                endtime: Timestamp = Timestamp(year + 1, 1, 1, 0, 0, 0)
+                history = site.loadrevisions(page=rfc_page, user='Legobot', starttime=starttime, endtime=endtime, rvdir=True)
 
-    # Will print revision entries where the comment contains 'added'
-    #  * revid: 1330081275
-    # * timestamp: 2025-12-29T10:01:21Z
-    # * user: Legobot
-    # * comment: Added: [[Talk:Denis Kapustin (militant)]].
-    for entry in (rfc_page._revisions or {}).values():
-        print_keys = ['revid', 'parentid', 'timestamp', 'user', 'comment']
+            # TODO: Do I need to access _revisions or is there a better way?
+
+            # Will print revision entries where the comment contains 'added'
+            #  * revid: 1330081275
+            # * timestamp: 2025-12-29T10:01:21Z
+            # * user: Legobot
+            # * comment: Added: [[Talk:Denis Kapustin (militant)]].
+            for entry in (rfc_page._revisions or {}).values():
+                save_revision(db, run, entry)
+            stats_key, stats_value = run.get_complete_stats()
+            db[stats_key] = stats_value
+            print(f"Completed examining history. Stats: {stats_value}")
+            db.close()
+
+def save_revision(db: shelve.Shelf[any], run: RevisionRun, entry: Revision):
+    try:
+        revision_key = f"revision-{entry.revid}"
+        entry['processed'] = False
+        db[revision_key] = entry
+        db.sync()
+        diff_table = site.compare(entry.get('parentid'), entry.get('revid'),'table')
+        revision_details = {
+            'revid': entry.revid,
+            'parentid': entry.parentid,
+            'timestamp': entry.timestamp,
+            'user': entry.user,
+            'comment': entry.comment,
+            'diff_table': diff_table
+        }
+        db[f"revision-details-{entry.revid}"] = revision_details
+        entry['processed'] = True
+        db[revision_key] = entry
+        run.increment_revisions_examined()
+
+    # catch any exception
+    except Exception as e:
+        print(f"Error saving revision {entry.revid}: {e}")
+    finally:
+        db.sync()
+    
+    db[run.get_key()] = run
+
+def handle_entry(entry):
+    print_keys = ['revid', 'parentid', 'timestamp', 'user', 'comment']
         # added or Removed
-        if 'removed' in entry.get('comment', '').lower():
-            comment = entry.get('comment')
+    if 'removed' in entry.get('comment', '').lower():
+        print_removed_entries(entry, print_keys)
+
+def print_removed_entries(entry, print_keys):
+    comment = entry.get('comment')
             # Delete everything before "[["
-            if "[[" in comment:
-                comment = comment[comment.index("[["):]
+    if "[[" in comment:
+        comment = comment[comment.index("[["):]
             # remove any trailing period
-            if comment.endswith('.'):
-                comment = comment[:-1]
+    if comment.endswith('.'):
+        comment = comment[:-1]
 
-            print(f'== {comment} ==')
-            revision_comment = '<!-- dwalphabot=1,'
-            for key in print_keys:
+    print(f'== {comment} ==')
+    revision_comment = '<!-- dwalphabot=1,'
+    for key in print_keys:
                 #print(f"* {key}: {entry.get(key)}")
-                revision_comment += f"{key}={str(entry.get(key))},"
-            revision_comment += ' -->\n'
-            print(revision_comment)
-            diff_table = site.compare(entry.get('parentid'), entry.get('revid'),'table') # works better than 'inline'
-            diff_compare = html_comparator(diff_table)
+        revision_comment += f"{key}={str(entry.get(key))},"
+    revision_comment += ' -->\n'
+    print(revision_comment)
+    diff_table = site.compare(entry.get('parentid'), entry.get('revid'),'table') # works better than 'inline'
+    diff_compare = html_comparator(diff_table)
 
-            deleted_content = diff_compare['deleted-context'] or []
-            deleted_lines = '\n'.join(deleted_content)
+    deleted_content = diff_compare['deleted-context'] or []
+    deleted_lines = '\n'.join(deleted_content)
 
-            print(deleted_lines)
-            print("")
+    print(deleted_lines)
+    print("")
 
 
 
